0e5e5550afcb5c3087e5858974764f4fc8d6c4de
quyan <quyan2004@gmail.com>
2024-09-25 18:01:18 +0700

    a

--- src/index.js
+++ src/index.js
@@ -10,6 +10,19 @@ root.render(
     <App />
   </React.StrictMode>
 );
+document.addEventListener('DOMContentLoaded', (event) => {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  } else {
+    console.error('Target container is not a DOM element.');
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
